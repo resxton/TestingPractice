1a17b38e94a21ffc160ae8e609d4f5670aa79a17
Dmitriy Toropkin <d.toropkin@tinkoff.ru>
2025-05-05 17:08:05 +0300

    Initial Commit

---
+++ DemoAppForTestsUITests/DemoAppForTestsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  DemoAppForTestsUITestsLaunchTests.swift
+//  DemoAppForTestsUITests
+//
+//  Created by Dmitriy Toropkin on 05.05.2025.
+//
+
+import XCTest
+
+final class DemoAppForTestsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
